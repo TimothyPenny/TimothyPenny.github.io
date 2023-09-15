05219981fef69c3180713334ac6f163a7f0f2723
Timothy Penny <timothy@nolapenny.com>
2023-09-15 17:44:40 +0000

    add index.html and css files

--- projects/platformer/helpers.js
+++ projects/platformer/helpers.js
@@ -368,14 +368,14 @@ function deathOfPlayer() {
   ctx.fillStyle = "black";
   ctx.font = "800% serif";
   ctx.fillText(
-    "You are dead",
+    "skill issue",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 5,
     (canvas.width / 16) * 14
   );
-  ctx.font = "500% serif";
+  ctx.font = "200% serif";
   ctx.fillText(
-    "Hit any key to restart",
+    "Hot dog buns are for fridays only",
     canvas.width / 4,
     canvas.height / 6 + canvas.height / 3,
     (canvas.width / 16) * 14
